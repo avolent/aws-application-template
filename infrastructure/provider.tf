82b445f8fa8d1813508c3e15bc58925d1af15eb4
Will <40684390+avolent@users.noreply.github.com>
2023-07-15 14:21:38 +1000

    fix(ci): disable plan for template (#6)

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -2,8 +2,9 @@ provider "aws" {
   region = var.aws_region
   default_tags {
     tags = {
-      Environment = var.environment
-      Project     = var.project_name
+      Deployed_from = var.git_repo
+      Environment   = var.environment
+      Project       = var.project_name
     }
   }
 }
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,13 +1,18 @@
-variable "project_name" {
-  type        = string
-  description = "Your project name, it will be used for naming resources"
-  default     = "application_name"
-}
-
 variable "aws_region" {
   type        = string
   description = "The default aws region"
   default     = "ap-southeast-2"
+}
+
+variable "git_repo" {
+  type        = string
+  description = "The current git repository"
+}
+
+variable "project_name" {
+  type        = string
+  description = "Your project name, it will be used for naming resources"
+  default     = "application_name"
 }
 
 variable "environment" {
